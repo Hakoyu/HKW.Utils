602878796eca38b420e7f7d69537cd24e86ba065
Hakoyu <Hakoyu@outlook.com>
2023-09-17 18:14:19 +0800

    更新

--- HKW.Utils/Extensions/IEnumerable{T}/SequenceEqual.cs
+++ HKW.Utils/Extensions/IEnumerable{T}/SequenceEqual.cs
@@ -9,11 +9,11 @@ public static partial class HKWExtensions
     /// 可使用匿名方法作为比较器的序列相等
     /// </summary>
     /// <typeparam name="T">类型</typeparam>
-    /// <param name="first">第一个枚举</param>
-    /// <param name="second">第二个枚举</param>
+    /// <param name="first">第一个集合</param>
+    /// <param name="second">第二个集合</param>
     /// <param name="comparer">比较器</param>
     /// <returns>相等为 <see langword="true"/> 不相等为 <see langword="false"/></returns>
-    public static bool SequenceEqualOnComparer<T>(
+    public static bool SequenceEqual<T>(
         this IEnumerable<T> first,
         IEnumerable<T> second,
         Func<T, T, bool> comparer
