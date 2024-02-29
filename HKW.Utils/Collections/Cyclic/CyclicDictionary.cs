72385075d7928b404d549758cf5c0964ea036045
Hakoyu <Hakoyu@outlook.com>
2024-02-29 22:06:34 +0800

    # 新增 - `CyclicList` - `CyclicDictionary`

---
+++ HKW.Utils/Collections/Cyclic/CyclicDictionary.cs
@@ -0,0 +1,221 @@
+﻿using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using HKW.HKWUtils.Extensions;
+
+namespace HKW.HKWUtils.Collections;
+
+/// <summary>
+/// 循环字典
+/// </summary>
+/// <typeparam name="TKey">键类型</typeparam>
+/// <typeparam name="TValue">值类型</typeparam>
+public class CyclicDictionary<TKey, TValue> : IDictionary<TKey, TValue>
+    where TKey : notnull
+{
+    private readonly Dictionary<TKey, TValue> _dictionary;
+
+    #region ctor
+    /// <inheritdoc/>
+    public CyclicDictionary()
+        : this(null!, null) { }
+
+    /// <inheritdoc/>
+    /// <param name="collection">键值对集合</param>
+    public CyclicDictionary(IEnumerable<KeyValuePair<TKey, TValue>> collection)
+        : this(collection, null) { }
+
+    /// <inheritdoc/>
+    /// <param name="comparer">比较器</param>
+    public CyclicDictionary(IEqualityComparer<TKey> comparer)
+        : this(null!, comparer) { }
+
+    /// <inheritdoc/>
+    /// <param name="collection">键值对集合</param>
+    /// <param name="comparer">比较器</param>
+    public CyclicDictionary(
+        IEnumerable<KeyValuePair<TKey, TValue>> collection,
+        IEqualityComparer<TKey>? comparer
+    )
+    {
+        if (collection is not null)
+            _dictionary = new(collection, comparer);
+        else
+            _dictionary = new(comparer);
+    }
+    #endregion
+
+    #region Cyclic
+
+    private KeyValuePair<TKey, TValue> _current = default;
+
+    /// <summary>
+    /// 当前项目
+    /// </summary>
+    public KeyValuePair<TKey, TValue> Current
+    {
+        get => _current;
+        private set
+        {
+            _current = value;
+            CurrentKey = _current.Key;
+            CurrentValue = _current.Value;
+        }
+    }
+
+    /// <summary>
+    /// 当前索引
+    /// </summary>
+    public TKey CurrentKey { get; private set; } = default!;
+
+    /// <summary>
+    /// 当前值
+    /// </summary>
+    public TValue CurrentValue { get; private set; } = default!;
+
+    /// <summary>
+    /// 自动重置
+    /// </summary>
+    [DefaultValue(false)]
+    public bool AutoReset { get; set; } = false;
+
+    private readonly List<TKey> _keys = new();
+    private int _currentIndex = 0;
+
+    /// <summary>
+    /// 移动到下一个
+    /// </summary>
+    /// <returns>移动成功为 <see langword="true"/> 失败为 <see langword="false"/></returns>
+    public bool MoveNext()
+    {
+        if (_currentIndex >= Count)
+        {
+            if (AutoReset)
+            {
+                _currentIndex = 0;
+                Current = _dictionary.GetPair(_keys[_currentIndex]);
+                return true;
+            }
+            return false;
+        }
+        _currentIndex++;
+        Current = _dictionary.GetPair(_keys[_currentIndex]);
+        return true;
+    }
+
+    /// <summary>
+    /// 重置循环
+    /// </summary>
+    public void Reset()
+    {
+        if (Count == 0)
+        {
+            _currentIndex = -1;
+            Current = default!;
+        }
+        else
+        {
+            _currentIndex = 0;
+            Current = _dictionary.First();
+        }
+    }
+    #endregion
+
+    #region IDictionary
+    /// <inheritdoc/>
+    public TValue this[TKey key]
+    {
+        get => ((IDictionary<TKey, TValue>)_dictionary)[key];
+        set => ((IDictionary<TKey, TValue>)_dictionary)[key] = value;
+    }
+
+    /// <inheritdoc/>
+    public ICollection<TKey> Keys => ((IDictionary<TKey, TValue>)_dictionary).Keys;
+
+    /// <inheritdoc/>
+    public ICollection<TValue> Values => ((IDictionary<TKey, TValue>)_dictionary).Values;
+
+    /// <inheritdoc/>
+    public int Count => ((ICollection<KeyValuePair<TKey, TValue>>)_dictionary).Count;
+
+    /// <inheritdoc/>
+    public bool IsReadOnly => ((ICollection<KeyValuePair<TKey, TValue>>)_dictionary).IsReadOnly;
+
+    /// <inheritdoc/>
+    public void Add(TKey key, TValue value)
+    {
+        ((IDictionary<TKey, TValue>)_dictionary).Add(key, value);
+        _keys.Add(key);
+        Reset();
+    }
+
+    /// <inheritdoc/>
+    public void Add(KeyValuePair<TKey, TValue> item)
+    {
+        ((ICollection<KeyValuePair<TKey, TValue>>)_dictionary).Add(item);
+        _keys.Add(item.Key);
+        Reset();
+    }
+
+    /// <inheritdoc/>
+    public void Clear()
+    {
+        ((ICollection<KeyValuePair<TKey, TValue>>)_dictionary).Clear();
+        _keys.Clear();
+        Reset();
+    }
+
+    /// <inheritdoc/>
+    public bool Contains(KeyValuePair<TKey, TValue> item)
+    {
+        return ((ICollection<KeyValuePair<TKey, TValue>>)_dictionary).Contains(item);
+    }
+
+    /// <inheritdoc/>
+    public bool ContainsKey(TKey key)
+    {
+        return ((IDictionary<TKey, TValue>)_dictionary).ContainsKey(key);
+    }
+
+    /// <inheritdoc/>
+    public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
+    {
+        ((ICollection<KeyValuePair<TKey, TValue>>)_dictionary).CopyTo(array, arrayIndex);
+    }
+
+    /// <inheritdoc/>
+    public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+    {
+        return ((IEnumerable<KeyValuePair<TKey, TValue>>)_dictionary).GetEnumerator();
+    }
+
+    /// <inheritdoc/>
+    public bool Remove(TKey key)
+    {
+        var result = ((IDictionary<TKey, TValue>)_dictionary).Remove(key);
+        _keys.Remove(key);
+        Reset();
+        return result;
+    }
+
+    /// <inheritdoc/>
+    public bool Remove(KeyValuePair<TKey, TValue> item)
+    {
+        var result = ((ICollection<KeyValuePair<TKey, TValue>>)_dictionary).Remove(item);
+        _keys.Remove(item.Key);
+        Reset();
+        return result;
+    }
+
+    /// <inheritdoc/>
+    public bool TryGetValue(TKey key, [MaybeNullWhen(false)] out TValue value)
+    {
+        return ((IDictionary<TKey, TValue>)_dictionary).TryGetValue(key, out value);
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return ((IEnumerable)_dictionary).GetEnumerator();
+    }
+    #endregion
+}
---
+++ HKW.Utils/Collections/Cyclic/CyclicList.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HKW.HKWUtils.Collections;
+
+/// <summary>
+/// 循环列表
+/// <para>任何修改列表的行为会导致循环重置</para>
+/// </summary>
+/// <typeparam name="T">项目类型</typeparam>
+public class CyclicList<T> : IListX<T>
+{
+    private readonly List<T> _list;
+
+    #region ctor
+    /// <inheritdoc/>
+    public CyclicList()
+        : this(null, null) { }
+
+    /// <inheritdoc/>
+    /// <param name="capacity">容量</param>
+    public CyclicList(int capacity)
+        : this(capacity, null) { }
+
+    /// <inheritdoc/>
+    /// <param name="collection">集合</param>
+    public CyclicList(IEnumerable<T> collection)
+        : this(null, collection) { }
+
+    /// <inheritdoc/>
+    /// <param name="capacity">容量</param>
+    /// <param name="collection">集合</param>
+    private CyclicList(int? capacity, IEnumerable<T>? collection)
+    {
+        if (capacity is not null)
+            _list = new(capacity.Value);
+        else if (collection is not null)
+            _list = new(collection);
+        else
+            _list = new();
+    }
+    #endregion
+
+    #region Cyclic
+    /// <summary>
+    /// 当前项目
+    /// </summary>
+    public T Current { get; private set; } = default!;
+
+    /// <summary>
+    /// 当前索引
+    /// </summary>
+    public int CurrntIndex { get; private set; } = 0;
+
+    /// <summary>
+    /// 自动重置
+    /// </summary>
+    [DefaultValue(false)]
+    public bool AutoReset { get; set; } = false;
+
+    /// <summary>
+    /// 移动到下一个
+    /// </summary>
+    /// <returns>移动成功为 <see langword="true"/> 失败为 <see langword="false"/></returns>
+    public bool MoveNext()
+    {
+        if (CurrntIndex >= _list.Count)
+        {
+            if (AutoReset)
+            {
+                CurrntIndex = 0;
+                Current = _list[CurrntIndex];
+                return true;
+            }
+            return false;
+        }
+        CurrntIndex++;
+        Current = _list[CurrntIndex];
+        return true;
+    }
+
+    /// <summary>
+    /// 重置循环
+    /// </summary>
+    public void Reset()
+    {
+        if (Count == 0)
+        {
+            CurrntIndex = -1;
+            Current = default!;
+        }
+        else
+        {
+            CurrntIndex = 0;
+            Current = _list[CurrntIndex];
+        }
+    }
+    #endregion
+
+    #region IList
+    /// <inheritdoc/>
+    public T this[int index]
+    {
+        get => ((IList<T>)_list)[index];
+        set => ((IList<T>)_list)[index] = value;
+    }
+
+    /// <inheritdoc/>
+    public int Count => ((ICollection<T>)_list).Count;
+
+    /// <inheritdoc/>
+    public bool IsReadOnly => ((ICollection<T>)_list).IsReadOnly;
+
+    /// <inheritdoc/>
+    public void Add(T item)
+    {
+        ((ICollection<T>)_list).Add(item);
+        Reset();
+    }
+
+    /// <inheritdoc/>
+    public void Clear()
+    {
+        ((ICollection<T>)_list).Clear();
+        Reset();
+    }
+
+    /// <inheritdoc/>
+    public bool Contains(T item)
+    {
+        return ((ICollection<T>)_list).Contains(item);
+    }
+
+    /// <inheritdoc/>
+    public void CopyTo(T[] array, int arrayIndex)
+    {
+        ((ICollection<T>)_list).CopyTo(array, arrayIndex);
+    }
+
+    /// <inheritdoc/>
+    public IEnumerator<T> GetEnumerator()
+    {
+        return ((IEnumerable<T>)_list).GetEnumerator();
+    }
+
+    /// <inheritdoc/>
+    public int IndexOf(T item)
+    {
+        return ((IList<T>)_list).IndexOf(item);
+    }
+
+    /// <inheritdoc/>
+    public void Insert(int index, T item)
+    {
+        ((IList<T>)_list).Insert(index, item);
+        Reset();
+    }
+
+    /// <inheritdoc/>
+    public bool Remove(T item)
+    {
+        var result = ((ICollection<T>)_list).Remove(item);
+        Reset();
+        return result;
+    }
+
+    /// <inheritdoc/>
+    public void RemoveAt(int index)
+    {
+        ((IList<T>)_list).RemoveAt(index);
+        Reset();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return ((IEnumerable)_list).GetEnumerator();
+    }
+    #endregion
+
+    #region IListX
+    /// <inheritdoc/>
+    public void AddRange(IEnumerable<T> collection)
+    {
+        _list.AddRange(collection);
+        Reset();
+    }
+
+    /// <inheritdoc/>
+    public void InsertRange(int index, IEnumerable<T> collection)
+    {
+        _list.InsertRange(index, collection);
+        Reset();
+    }
+
+    /// <inheritdoc/>
+    public void RemoveAll(Predicate<T> match)
+    {
+        _list.RemoveAll(match);
+        Reset();
+    }
+
+    /// <inheritdoc/>
+    public void RemoveRange(int index, int count)
+    {
+        _list.RemoveRange(index, count);
+        Reset();
+    }
+
+    /// <inheritdoc/>
+    public void Reverse()
+    {
+        _list.Reverse();
+        Reset();
+    }
+
+    /// <inheritdoc/>
+    public void Reverse(int index, int count)
+    {
+        _list.Reverse(index, count);
+        Reset();
+    }
+    #endregion
+}
