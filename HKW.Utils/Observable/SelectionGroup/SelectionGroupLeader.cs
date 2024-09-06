88e9bf1934d40b031e25c477bf88d2c988095a72
Hakoyu <Hakoyu@outlook.com>
2024-09-06 23:20:27 +0800

    新增 `SelectionGroupLeader`

---
+++ HKW.Utils/Observable/SelectionGroup/SelectionGroupLeader.cs
@@ -0,0 +1,28 @@
+﻿using HKW.HKWReactiveUI;
+
+namespace HKW.HKWUtils.Observable;
+
+/// <summary>
+/// 可选择组队长
+/// </summary>
+public partial class SelectionGroupLeader : ReactiveObjectX
+{
+    /// <inheritdoc/>
+    /// <param name="isSelected">已选中</param>
+    public SelectionGroupLeader(bool isSelected = false)
+    {
+        IsSelected = isSelected;
+        Wrapper = new(this, nameof(IsSelected), x => x.IsSelected, (x, v) => x.IsSelected = v);
+    }
+
+    /// <summary>
+    /// 已选中
+    /// </summary>
+    [ReactiveProperty]
+    public bool? IsSelected { get; set; }
+
+    /// <summary>
+    /// 转换器
+    /// </summary>
+    public ObservablePropertyWrapper<SelectionGroupLeader, bool?> Wrapper { get; }
+}
