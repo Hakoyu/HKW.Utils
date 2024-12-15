9689777801fc0f7b7c8c37bde9815360a3742f08
Hakoyu <Hakoyu@outlook.com>
2024-12-16 01:05:33 +0800

    新增`IResettable`

---
+++ HKW.Utils/Utils/IResettable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HKW.HKWUtils;
+
+/// <summary>
+/// 可重置接口, 用于需要复用的类型中
+/// </summary>
+public interface IResettable
+{
+    /// <summary>
+    /// 重置
+    /// </summary>
+    public void Reset();
+}
